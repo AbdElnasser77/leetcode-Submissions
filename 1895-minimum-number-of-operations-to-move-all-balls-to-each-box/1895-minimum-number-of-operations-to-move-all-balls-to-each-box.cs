3ce1b4adc233d45ac8694bc00df41f0e01d44ee3
Ahmed AbdElnasser <133560043+AbdElnasser77@users.noreply.github.com>
2025-04-08 12:18:17 +0200

    Time: 61 ms (39.39%), Space: 51.3 MB (65.66%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cs
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cs
@@ -1,21 +1,22 @@
 public class Solution {
     public int[] MinOperations(string boxes) {
-         int[] answer = new int[boxes.Length];
+  int n = boxes.Length;
+int[] answer = new int[n];
 
- for(int i = 0; i < boxes.Length ; i++)
- {
-     int sum = 0;
-     for(int j = 0; j < boxes.Length; j++)
-     {
-             if(boxes[j] == '1')
-             {
-                 sum += Math.Abs(j-i);
-             }
-     }
-     answer[i] = sum;
- }
+for(int i = 0; i < n; i++)
+{
+    int sum = 0;
+    for(int j = 0; j < n; j++)
+    {
+            if(boxes[j] == '1')
+            {
+                sum += Math.Abs(j-i);
+            }
+    }
+    answer[i] = sum;
+}
 
 
- return answer;
+return answer;
     }
 }
