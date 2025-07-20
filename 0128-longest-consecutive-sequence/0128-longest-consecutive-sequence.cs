fbe13d4854481903f1cb5ac5a42ab11d47132f08
Ahmed AbdElnasser <133560043+AbdElnasser77@users.noreply.github.com>
2025-07-20 04:21:34 +0300

    Time: 41 ms (89.83%), Space: 73.6 MB (56.88%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cs
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cs
@@ -1,23 +1,21 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) {
-        Array.Sort(nums);
+          var hashSet = new HashSet<int>(nums);
+   int max = 0;
 
-        int counter = 0;
-        int maxCount = 0;
-        for (int i = 1 ; i < nums.Length ; i++)
-        {
-            if (nums[i - 1] == nums[i] - 1) counter++;
-            else if(nums[i - 1] == nums[i]) continue;
-            else
-            {
-                if (maxCount < counter) maxCount = counter;
-                counter = 0;
-            }
-        }
-        if( nums.Length == 0) return 0;
-        else if (nums.Length == 1) return 1;
-        else if(counter>maxCount) return counter+1;
-        else
-        return maxCount+1;
+   foreach(var num in hashSet)
+   {
+       if (!hashSet.Contains(num - 1))
+       {
+           int currentNum = num;
+           while(hashSet.Contains(currentNum + 1))
+           {
+               currentNum++;
+           }
+           max = Math.Max(currentNum-num + 1,max);
+       }
+   }    
+
+   return max;
     }
 }
